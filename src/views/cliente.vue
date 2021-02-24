71a9a78ce02a092f5f3e6ecbd9bfa5d764ab00b4
joao-belmiro <joao_belmiro@hotmail.com>
2021-02-23 23:15:34 -0300

    form cliente

--- src/views/cliente.vue
+++ src/views/cliente.vue
@@ -1,31 +1,138 @@
 <template>
-<div class="q-pa-md">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6 text-grey-7">Pesquisar Cliente</div>
-          <q-separator/>
-        </q-card-section>
-        <q-card-section>
-        </q-card-section>
-        <q-card-section>
-          <div class="row q-gutter-md justify-center">
-            <q-btn
+  <div class="q-pa-md q-gutter-md">
+     <q-breadcrumbs class="text-dark" active-color="secondary">
+      <template v-slot:separator>
+        <q-icon
+          size="1.5em"
+          name="chevron_right"
+          color="secondary"
+        />
+      </template>
+      <q-breadcrumbs-el active-class="tezt-dark" label="Home" icon="home" to="home" />
+      <q-breadcrumbs-el label="pesquisar Cliente" icon="search" />
+    </q-breadcrumbs>
+    <q-card>
+      <q-card-section >
+        <div class=" row items-center text-h6 text-white bg-secondary q-px-md rounded-borders">
+          <q-icon class="q-pr-sm" name="search" />
+          Pesquisar Cliente</div>
+      </q-card-section>
+      <q-card-section>
+        <div class="column  ">
+            <q-input dense  label="Nome" color="secondary">
+            </q-input>
+           <div class=" q-pt-sm">
+          <q-option-group
+          v-model="tipo"
+          :options="tipos"
+          color="secondary"
+          inline
+          />
+        </div>
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <div class="row q-gutter-md">
+          <q-btn
+            dense
             label="Pesquisar"
             icon="search"
-            color="positive"/>
+            color="positive"
+            no-caps
+          />
           <q-btn
+            dense
             label="limpar"
             icon="fas fa-times"
-            color="grey"/>
-          </div>
+            color="grey"
+            no-caps
+          />
+          <q-btn dense label="Novo " icon="add" color="blue" no-caps @click="formCliente = !formCliente" />
+        </div>
+      </q-card-section>
+    </q-card>
+    <q-dialog v-model="icon">
+      <q-card>
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-h6">Close icon</div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <q-card-section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
         </q-card-section>
       </q-card>
-</div>
+    </q-dialog>
+
+    <q-dialog v-model="formCliente" persistent>
+      <q-card>
+        <div class="row q-pa-md">
+        <div class="text-h6 text-grey-7">
+          Cadastrar Cliente
+        </div>
+          <q-space />
+          <q-btn dense round flat icon="close" v-close-popup>
+            <q-tooltip>Fechar</q-tooltip>
+          </q-btn>
+        </div>
+        <q-card-section>
+           <div class="row items-start  q-col-gutter-md">
+          <div class="col-12 col-md-12 col-xs-12">
+            <q-input dense outlined v-model="cliente.nome" label="Nome" color="secondary">
+            </q-input>
+          </div>
+          <div class="col-12 col-md-12 col-xs-12">
+            <q-input dense outlined label="E-mail" v-model="cliente.email"  type="email" color="secondary">
+            </q-input>
+          </div>
+          <div class="col-12 col-md-12 col-xs-12">
+            <q-input dense outlined label="Telefone" mask="(##) #####-####" v-model="cliente.telefone" unmasked-value color="secondary">
+            </q-input>
+          </div>
+           <div class="q-pr-sm q-pt-sm">
+          <q-option-group
+          v-model="cliente.tipo"
+          :options="tipos"
+          color="secondary"
+          inline
+          />
+        </div>
+           </div>
+        </q-card-section>
+
+        <q-card-actions align="right" class="q-px-md q-pb-md">
+        <div class="q-gutter-md">
+          <q-btn label="Salvar" color="positive" text-color="white" icon="save" no-caps></q-btn>
+          <q-btn label="Cancelar" color="negative" text-color="white" icon="close" no-caps></q-btn>
+        </div>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      formCliente: false,
+      tipo: null,
+      tipos: [
+        {
+          label: 'Docente',
+          value: 'DO'
+        },
+        {
+          label: 'Discente',
+          value: 'DI'
+        }
+      ],
+      cliente: {
+        nome: null,
+        email: null,
+        telefone: null,
+        tipo: null
+      }
     }
   }
 }
--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,24 +1,58 @@
 <template>
-  <q-layout class="bg-grey-1">
+  <q-layout view="lHh Lpr lff" class="bg-grey-1">
     <q-header elevated class="text-white" style="background: #24292e">
-      <q-toolbar class="q-py-sm ">
-        <div
-          class="q-col-gutter-sm text-body2 text-weight-regular row items-center"
-        >
-          <q-item
-            clickable
-            v-ripple
-            tag="a"
-            to="/home"
-            active-class="my-menu-link"
+      <q-toolbar class="q-py-sm">
+        <router-link to="/home/" tag="div" class="cursor-pointer">
+          <div class="text-white row items-center text-h6">
+            <div>Bem-Vindo ao Refy</div>
+          </div>
+        </router-link>
+        <q-space />
+        <div class="q-pl-sm q-gutter-sm row items-center no-wrap">
+          <q-btn-dropdown
+            flat
+            dense
+            icon="fa fa-user"
+            color="white"
+            size="10px"
           >
-            <q-item-section avatar>
-              <q-icon name="fas fa-home" size="18px" />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Home</q-item-label>
-            </q-item-section>
-          </q-item>
+            <div class="row no-wrap q-pa-md">
+              <div class="column items-center">
+                <q-avatar size="72px">
+                  <q-icon name="fas fa-user-alt" />
+                </q-avatar>
+                <div class="text-subtitle1 q-mt-md q-mb-xs">{{ "joao" }}</div>
+                <div class="text-subtitle1 q-mb-xs">{{ "admin" }}</div>
+                <q-btn
+                  icon="fas fa-sign-out-alt"
+                  color="positive"
+                  label="Sair"
+                  size="md"
+                  v-close-popup
+                  to="/"
+                />
+              </div>
+            </div>
+          </q-btn-dropdown>
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      :width="250"
+      :breakpoint="400"
+      content-class="bg-grey-3"
+    >
+      <q-scroll-area
+        style="
+          height: calc(100% - 150px);
+          margin-top: 150px;
+          border-right: 1px solid #ddd;
+        "
+      >
+          <q-separator/>
+        <q-list >
           <q-item
             clickable
             v-ripple
@@ -31,7 +65,7 @@
             </q-item-section>
             <q-item-section>
               <q-item-label>Cliente</q-item-label>
-              <q-item-label class="text-white" caption>Gerenciamento de Cartões</q-item-label>
+              <q-item-label caption>Gerenciamento de Cartões</q-item-label>
             </q-item-section>
           </q-item>
           <q-item
@@ -46,34 +80,20 @@
             </q-item-section>
             <q-item-section>
               <q-item-label>Administrator</q-item-label>
+              <q-item-label caption
+                >Controle de Operadores do Sistema
+              </q-item-label>
             </q-item-section>
           </q-item>
-        </div>
-        <q-space />
-        <div class="q-pl-sm q-gutter-sm row items-center no-wrap">
-           <q-btn-dropdown flat dense icon="fa fa-user" color="white" size="10px">
-          <div class="row no-wrap q-pa-md">
-        <div class="column items-center">
-          <q-avatar size="72px">
-            <q-icon name="fas fa-user-alt"/>
-          </q-avatar>
-          <div class="text-subtitle1 q-mt-md q-mb-xs">{{'joao'}}</div>
-          <div class="text-subtitle1 q-mb-xs">{{'admin'}}</div>
-          <q-btn
-            icon="fas fa-sign-out-alt"
-            color="positive"
-            label="Sair"
-            size="md"
-            v-close-popup
-            to="/"
-          />
+           <q-separator/>
+        </q-list>
+      </q-scroll-area>
+      <div class="absolute-top" style="height: 150px">
+        <div class="col-sm-12 col-md-12 col-xs-12 text-center q-pt-md">
+          <q-icon color="grey-7" size="60px" name="fas fa-id-badge"></q-icon>
         </div>
       </div>
-        </q-btn-dropdown>
-        </div>
-      </q-toolbar>
-    </q-header>
-
+    </q-drawer>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -84,7 +104,9 @@
 export default {
   name: 'layout',
   data () {
-    return {}
+    return {
+      drawer: true
+    }
   },
   methods: {}
 }
